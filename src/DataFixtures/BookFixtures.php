ed8f892a61a08f1f4e36ebde8a3097ef82e31f26
Nono <Admin@MacBook-Pro-de-Test.local>
2021-05-31 14:54:49 +0200

    user fixtures

--- src/DataFixtures/BookFixtures.php
+++ src/DataFixtures/BookFixtures.php
@@ -3,15 +3,58 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Books;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class BookFixtures extends Fixture
 {
+    //gestion du hash de password
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder =  $encoder;
+    }
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
+
+        //gestion des utilisateurs
+        $users = []; //initialisation d'un tableau pour associer Books et User
+        $genres = ['male', 'femelle'];
+
+        for($u=1; $u <=10; $u++){
+            $user = new User();
+            $genre = $faker->randomElement($genres);
+
+            $picture= 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1,99).'.jpg';
+            $picture .=($genre == 'male' ? 'men/' : 'women/').$pictureId;
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+
+            $user->setFirstName($faker->firstName($genre))
+                ->setLastName($faker->lastName())
+                ->setEmail($faker->email())
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'.join('<p></p>', $faker->paragraphs(3)).'</p>')
+                ->setPassword($hash)
+                ->setPicture($picture);
+
+                $manager->persist($user);
+                $users[]= $user; //ajouter l'utilisateur fraichement créé dans le tableau pour l'association avec les annonces
+        }
+
+
+
+
+
+
+
+
 
         for($a =1; $a<=10; $a++){
             $books = new Books();
@@ -19,6 +62,7 @@ class BookFixtures extends Fixture
             $author = $faker->sentence($nbWords = 1, $variableNbWords = true);
             $genre = $faker->sentence($nbWords = 1, $variableNbWords = true);
             $resume = '<p>'.join('</p><p>',$faker->paragraphs(1)).'</p>';
+            $user = $users[rand(0,count($users)-1)];
             
 
             $books->setTitle($title)
@@ -26,7 +70,9 @@ class BookFixtures extends Fixture
                     ->setAuthor($author)
                     ->setGenre($genre)
                     ->setResume($resume)
-                    ->setPrice(rand(6,15));
+                    ->setPrice(rand(6,15))
+                    ->setUtilisateur($user)
+                    ;
                     
 
 
